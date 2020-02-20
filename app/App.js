39cd9bd1769352f034a54ae17f98f67eceefeeb3
Mostafa <mos.sameti@gmail.com>
2020-02-20 17:42:04 +0330

    add css baseline

--- app/App.js
+++ app/App.js
@@ -1,12 +1,29 @@
 import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
 import { BrowserRouter as Router } from "react-router-dom";
 import Routes from "./routes";
+import { CssBaseline } from '@material-ui/core';
+
+const useStyles = makeStyles(theme => ({
+  '@global': {
+    a: {
+      color: 'inherit',
+      textDecoration: 'none'
+    }
+  }
+}));
 
 export default function App() {
+  const classes = useStyles();
 
   return (
-    <Router>
-      <Routes />
-    </Router>
+    <>
+      <CssBaseline />
+      <main className={classes.root}>
+        <Router>
+          <Routes />
+        </Router>
+      </main>
+    </>
   );
 }
