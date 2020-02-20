7877ecfdd3ff700d85a03abdc7f17232a3e289b4
Mostafa <mos.sameti@gmail.com>
2020-02-20 20:43:01 +0330

    add single chat page

--- app/pages/single-chat.js
+++ app/pages/single-chat.js
@@ -1,5 +1,84 @@
-import React from "react";
+import React, { useState } from "react";
+import MainAnatomy from "@app/components/main-anatomy";
+import { makeStyles } from "@material-ui/core/styles";
+import { ListItem, Avatar, ListItemText } from "@material-ui/core";
+import { Link } from "react-router-dom";
+import SendMessageBox from "@app/components/send-message-box";
+import ChatMessages from "@app/components/chat-view";
 
-export default function SingleChat(props) {
-  return <h2>{props.name}</h2>;
+const useStyles = makeStyles(theme => ({
+  titleText: {
+    paddingLeft: theme.spacing(2)
+  },
+  chatBox: {
+    position: "relative",
+    minHeight: "calc(100vh - 100px)"
+  }
+}));
+
+export default function SingleChat() {
+  const classes = useStyles();
+  const [messageSending, setMessageSending] = useState(false);
+  const [chatData, setChatDate] = useState({
+    id: "1",
+    name: "John Doe",
+    phone: "09123334455",
+    userName: "john_doe",
+    messages: [
+      {
+        sender: "self",
+        content:
+          "This impressive paella is a perfect party dish and a fun meal to cook together with your guests. Add 1 cup of frozen peas along with the mussels, if you like",
+        date: "1582113303"
+      },
+      {
+        sender: "else",
+        content: "Hi!",
+        date: "1582213303"
+      }
+    ]
+  });
+
+  const handleSendMessage = (content, date) => {
+    setMessageSending(true);
+    setTimeout(() => {
+      setChatDate({
+        ...chatData,
+        messages: [
+          ...chatData.messages,
+          {
+            sender: "self",
+            content,
+            date
+          }
+        ]
+      });
+      setMessageSending(false);
+    }, 2000);
+  };
+
+  return (
+    <MainAnatomy
+      hasBackIcon
+      title={
+        <Link to={`/info?name=${chatData.userName}`}>
+          <ListItem button key="contacts">
+            <Avatar />
+            <ListItemText
+              className={classes.titleText}
+              primary={chatData.name}
+            />
+          </ListItem>
+        </Link>
+      }
+    >
+      <div className={classes.chatBox}>
+        <ChatMessages messages={chatData.messages} />
+        <SendMessageBox
+          loading={messageSending}
+          onSendMessage={handleSendMessage}
+        />
+      </div>
+    </MainAnatomy>
+  );
 }
