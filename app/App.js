9bdd6f099334cea99727d79dd98c5b148a3f35fb
Mostafa <mos.sameti@gmail.com>
2020-05-13 02:24:44 +0430

    add react-redux dependency and configs

--- app/App.js
+++ app/App.js
@@ -1,8 +1,12 @@
 import React from "react";
-import { makeStyles } from "@material-ui/core/styles";
 import { BrowserRouter as Router } from "react-router-dom";
+import { Provider } from 'react-redux'
+
 import Routes from "./routes";
+import store from './store';
+
 import { CssBaseline } from '@material-ui/core';
+import { makeStyles } from "@material-ui/core/styles";
 
 const useStyles = makeStyles(theme => ({
   '@global': {
@@ -17,13 +21,13 @@ export default function App() {
   const classes = useStyles();
 
   return (
-    <>
+    <Provider store={store}>
       <CssBaseline />
       <main className={classes.root}>
         <Router>
           <Routes />
         </Router>
       </main>
-    </>
+    </Provider>
   );
 }
